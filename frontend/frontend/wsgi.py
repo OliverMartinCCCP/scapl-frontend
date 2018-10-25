e0641aac831241d1d277e7f52e6ce0db4e21d944
OliverMartinCCCP <oliver.cccp@gmail.com>
2018-10-25 10:22:05 +0200

    Because we use Apache 2 as the web server we needed to make some changes. A virtual environment is created in order to make Django work together with Apache 2 and Python 3. Some system variables needed to be added as well to become accessible.

--- frontend/frontend/wsgi.py
+++ frontend/frontend/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
+import sys
+import site
+
+site.addsitedir('/scapl_environment/lib/python3.4/site-packages')
+sys.path.append('/opt/scapl/scapl-frontend/frontend')
+sys.path.append('/scapl_environment/lib/python3.4/site-packages')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings.dev")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
